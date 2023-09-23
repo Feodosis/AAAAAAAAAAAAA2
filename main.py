a10b2bbf290d00473107a7edd97281f53c55077b
Feodosis <144526217+Feodosis@users.noreply.github.com>
2023-09-23 13:00:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,34 +3,45 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
-from random import randint
+
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QMessageBox, QRadioButton
 app = QApplication([])
-my_win = QWidget()
-my_win.setWindowTitle("Визначник переможця")
-my_win.move(100, 100)
-my_win.resize(400, 200) #змінює розмір
-button = QPushButton(my_win)
-button.setText("Згенерувати")
-button.move(140, 130)
+win = QWidget()
+win.setWindowTitle('Конкурс від Crazy People')
+win.resize(400, 200)
+question = QLabel(win)
+question.setText('Якого року канал отримав золоту кнопку від Ютуб ?')
+question.move(50, 10)
 
-text = QLabel(my_win)
-text.setText("Натисніть, щоб дізнатись переможця")
-text.move(70, 10)
+button1 = QRadioButton(win)
+button1.setText('2005')
+button1.move(100, 60)
 
-winner = QLabel(my_win)
-winner.setText("?")
-winner.move(190, 70)
+button2 = QRadioButton(win)
+button2.setText('2010')
+button2.move(100, 110)
 
-def show_winner():
-    number = randint(1, 100)
-    winner.setText(str(number))
-    text.setText("Переможець:")
-    text.move(190, 10)
-button.clicked.connect(show_winner) #підключення функції
+button3 = QRadioButton(win)
+button3.setText('2015')
+button3.move(200, 60)
 
+button4 = QRadioButton(win)
+button4.setText('2020')
+button4.move(100, 90)
 
-my_win.show()
+def show_win():
+    win2 = QMessageBox()
+    win2.setText('Правильно! Ви виграли Монако')
+    win2.exec_()
+
+button3.clicked.connect(show_win)
+def show_lose():
+    win2 = QMessageBox()
+    win2.setText('Ні, в 2015 році. Ви виграли фірмовий плакат')
+    win2.exec_()
+button1.clicked.connect(show_lose)
+button2.clicked.connect(show_lose)
+button4.clicked.connect(show_lose)
+
+win.show()
 app.exec_()
-
-
